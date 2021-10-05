8a01fe66f270dcd2f5708b30693efc73e3edbfc9
rohit parghi <shilpa.excelsior@gmail.com>
2021-10-05 11:24:30 +0530

    business controller code update

--- app/Http/Controllers/Api/BusinessController.php
+++ app/Http/Controllers/Api/BusinessController.php
@@ -1307,8 +1307,7 @@ class BusinessController extends Controller
     public function getBusinessesCategoryWise(Request $request)
     {
         $input=$request->all();
-
-        if(!isset($input['CategoryId']) && empty($input['CategoryId']))
+        if(!isset($input['catagoryId']) && empty($input['catagoryId']))
         {
             $error[] = 'CategoryId Must be Required!';
         }
@@ -1318,8 +1317,7 @@ class BusinessController extends Controller
             return response()->json(['Status'=>false,'StatusMessage'=>implode(',',$error),'Result'=>array()]);
 		}
         
-        $businessArr=BusinessModel::where('category_id',$input['CategoryId'])->select('id','name')->get()->toArray();
-        
+        $businessArr=BusinessModel::where('category_id',$input['catagoryId'])->select('id','name')->get()->toArray();
         if(count($businessArr) > 0)
         {
             return response()->json(['Status'=>true,'StatusMessage'=>'Get business categorywise successfully !','Result'=>$businessArr]);
