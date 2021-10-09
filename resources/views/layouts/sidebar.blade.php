3f98d224161468da69839ace9f14251ddaa7cf2c
tarang pansuriya <tarang.excelsior@gmail.com>
2021-10-09 12:52:50 +0530

    admin sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,34 +1,42 @@
 @yield('css')
-<link href="{{ URL::asset('assets/css/stokeapp.min.css')}}" id="app-light" rel="stylesheet" type="text/css" />
-
-<!-- App Css-->
-        <!--<link href="https://themesbrand.com/skote/layouts/assets/css/app.min.css" id="app-style" rel="stylesheet" type="text/css" />--
+<link href="{{ URL::asset('assets/css/stokeapp.min.css') }}" id="app-light" rel="stylesheet" type="text/css" />
 
 <!-- ========== Left Sidebar Start ========== -->
 <div class="vertical-menu">
-   <div data-simplebar class="h-100">
-      <!--- Sidemenu -->
-      <div id="sidebar-menu">
-         <!-- Left Menu Start -->
-         <ul class="metismenu list-unstyled" id="side-menu">
-	
-            <li>
-               <a href="{{ url('admin/dashboard') }}" class="waves-effect">
-               <i class="bx bx-home-circle"></i>
-               <span key="t-dashboards">Dashboards</span>
-               </a>
-            </li>
-            <li class="menu-title">Modules</li>
-             <li>
+    <div data-simplebar class="h-100">
+        <!--- Sidemenu -->
+        <div id="sidebar-menu">
+            <!-- Left Menu Start -->
+            <ul class="metismenu list-unstyled" id="side-menu">
+                <!-- <li class="menu-title">Menu</li> -->
+                <li>
+                    <a href="{{ url('') }}" class="waves-effect">
+                        <i class="bx bx-home-circle"></i>
+
+                        <span>Dashboards</span>
+                    </a>
+
+                </li>
+
+                <li class="menu-title">Modules</li>
+
+                <!-- Quick Guide Sidebar End -->
+                <li>
                     <a href="{{ url('admin/business') }}"
-                        class="{{ Request::segment(2) === 'business.create' ? 'active' : '' }} ">
-						<i class="bx bx-briefcase-alt-2"></i><span>Business</span></a>
+                        class="{{ Request::segment(2) === 'business.create' ? 'active' : '' }} "><i
+                            class="mdi mdi-music-note-whole"></i>Business</a>
                 </li>
 
                 <li>
                     <a href="{{ url('admin/matrimonial') }}"
-                        class="{{ Request::segment(2) === 'matrimonial.create' ? 'active' : '' }} ">
-						<i class="bx bx-tone"></i><span>Matrimonial</span></a>
+                        class="{{ Request::segment(2) === 'matrimonial.create' ? 'active' : '' }} "><i
+                            class="mdi mdi-music-note-whole"></i>Matrimonial</a>
+                </li>
+
+                <li>
+                    <a href="{{ url('admin/faq') }}"
+                        class="{{ Request::segment(2) === 'faq.create' ? 'active' : '' }} "><i
+                            class="mdi mdi-music-note-whole"></i>FAQ</a>
                 </li>
 
                 <li>
@@ -37,19 +45,18 @@
                             class="mdi mdi-music-note-whole"></i>Fourm</a>
                 </li>
 
-
                 <li>
                     <a href="{{ url('admin/blog') }}"
                         class="{{ Request::segment(2) === 'blog.create' ? 'active' : '' }} "><i
                             class="mdi mdi-music-note-whole"></i>Blog</a>
                 </li>
+
                 <li>
                     <a href="{{ url('admin/user') }}"
                         class="{{ Request::segment(2) === 'user' ? 'active' : '' }} "><i
                             class="mdi mdi-music-note-whole"></i>User List</a>
                 </li>
-
-
+                <!-- Master -->
                 <li>
                     <a href="javascript: void(0);" class="has-arrow waves-effect ">
                         <i class="bx bx-check-double"></i>
@@ -57,13 +64,17 @@
                     </a>
                     <ul class="sub-menu" aria-expanded="false">
 
+
                         <li>
-                           <a href="{{ url('admin/country') }}"><i
-                              class="bx bx-radio-circle-marked"></i><span>Country</span></a>
+                            <a href="{{ url('admin/tags') }}"
+                                class="{{ Request::segment(2) === 'tags.create' ? 'active' : '' }} "><i
+                                    class="mdi mdi-music-note-whole"></i>Tags</a>
                         </li>
+
                         <li>
-                           <a href="{{ url('admin/city') }}"><i
-                              class="bx bx-radio-circle-marked"></i><span>City</span></a>
+                            <a href="{{ url('admin/tagsforum') }}"
+                                class="{{ Request::segment(2) === 'tagsforum.create' ? 'active' : '' }} "><i
+                                    class="mdi mdi-music-note-whole"></i>Tags Forum</a>
                         </li>
 
                         <li>
@@ -74,24 +85,24 @@
 
                         <li><a href="{{ url('admin/category') }}"
                                 class="{{ Request::segment(2) === 'category.create' ? 'active' : '' }} "><i
-                                    class="mdi mdi-music-note-whole"></i>Category</a></li>
-
-                                    <li>
-                                        <a href="javascript: void(0);" class="has-arrow waves-effect ">
-                                            <i class="fa fa-map-marker" style="font-size: 12px;" aria-hidden="true"></i>
-                                            <span>Location</span>
-                                        </a>
-                                        <ul class="sub-menu" aria-expanded="false">
-                                            <li>
-                                                <a href="{{ url('admin/country') }}"><i
-                                                        class="bx bx-radio-circle-marked"></i>Country</a>
-                                            </li>
-                                            <li>
-                                                <a href="{{ url('admin/city') }}"><i
-                                                        class="bx bx-radio-circle-marked"></i>City</a>
-                                            </li>
-                                        </ul>
-                                    </li>
+                                    class="mdi mdi-music-note-whole"></i>Category</a>
+                        </li>
+                        <li>
+                            <a href="javascript: void(0);" class="has-arrow waves-effect ">
+                                <i class="fa fa-map-marker" style="font-size: 12px;" aria-hidden="true"></i>
+                                <span>Location</span>
+                            </a>
+                            <ul class="sub-menu" aria-expanded="false">
+                                <li>
+                                    <a href="{{ url('admin/country') }}"><i
+                                            class="bx bx-radio-circle-marked"></i>Country</a>
+                                </li>
+                                <li>
+                                    <a href="{{ url('admin/city') }}"><i
+                                            class="bx bx-radio-circle-marked"></i>City</a>
+                                </li>
+                            </ul>
+                        </li>
 
 
                         <li>
@@ -103,10 +114,17 @@
                     </ul>
                 </li>
 
+
                 <li>
                     <a href="{{ url('admin/advertisement') }}"
                         class="{{ Request::segment(2) === 'advertisement.create' ? 'active' : '' }} "><i
                             class="mdi mdi-music-note-whole"></i>Advertisment</a>
+                </li>
+
+                <li>
+                    <a href="{{ url('admin/testimonial') }}"
+                        class="{{ Request::segment(2) === 'testimonial.create' ? 'active' : '' }} "><i
+                            class="mdi mdi-music-note-whole"></i>Testimonial</a>
                 </li>
 
                 <li>
@@ -115,11 +133,7 @@
                             class="mdi mdi-music-note-whole"></i>Notifications</a>
                 </li>
 
-                <li>
-                    <a href="{{ url('admin/testimonial') }}"
-                        class="{{ Request::segment(2) === 'testimonial.create' ? 'active' : '' }} "><i
-                            class="mdi mdi-music-note-whole"></i>Testmonial</a>
-                </li>
+                <!-- Setting Sidebar Start -->
 
             </ul>
         </div>
